3c1507ab54bc79e08eebb66c9d8c688a6d192752
keerthi <saikeerthi8978@gmail.com>
2019-03-20 23:06:22 +0530

    si push

---
+++ si/src/si/siTest.java
@@ -0,0 +1,19 @@
+package si;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class siTest {
+
+	@Test
+	void test() {
+		siclass obj=new siclass();
+		assertEquals(625.0, obj.calc(5000, 2.5, 5));
+
+		assertEquals(0.0, obj.calc(5000, 0, 5));
+
+		assertEquals(556.125, obj.calc(4449, 2.5, 5));
+	}
+
+}
---
+++ si/src/si/siclass.java
@@ -0,0 +1,12 @@
+package si;
+
+public class siclass {
+	public double calc(int p,double rate,double time)
+
+	{
+
+		return (p*rate*time)/100;
+
+	}
+
+}
